ae61b38ab1629d50c7d9190133e1334573598a6a
AleksandarKostovic <alexandar.kostovic@gmail.com>
2018-12-02 02:07:31 -0700

    Create up_counter.cpp

---
+++ examples/counter/up_counter.cpp
@@ -0,0 +1,11 @@
+#inlcude "up_counter.h"
+
+// main function for counter
+ void counter::counter_main (void) {
+    if (reset.read()) { 
+      count = 0 ;
+    } else if (enable.read()) {
+      count = count + 1;
+    }
+    out.write(count);
+  }
