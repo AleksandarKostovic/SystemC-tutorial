dacae9d4c07270f4ae98b488a8be07c6a7711b5f
AleksandarKostovic <alexandar.kostovic@gmail.com>
2018-12-02 01:44:26 -0700

    Update up_counter.cpp

--- examples/counter/up_counter.cpp
+++ examples/counter/up_counter.cpp
@@ -1,1 +1,28 @@
+#include <systemc.h>
 
+//Module counter
+SC_MODULE (up_down_counter) {
+  //input signals
+  sc_in    <bool> enable, clk, reset;
+  //output result
+  sc_out   <sc_uint<8> > out;
+  //internal variable
+  sc_uint<8>  count;
+
+  //up count function
+  void counter () {
+    if (reset.read()) { 
+      count = 0 ;
+    } else if (enable.read()) {
+      count = count + 1;
+    }
+    out.write(count);
+  }
+
+  //constructor phase
+  SC_CTOR(up_down_counter) {
+    SC_METHOD (counter);
+      sensitive << clk.pos();
+  }
+
+};
