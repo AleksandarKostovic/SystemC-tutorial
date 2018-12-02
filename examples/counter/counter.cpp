c2a3a98e68d4a3021efb514bf3996247720e032d
AleksandarKostovic <alexandar.kostovic@gmail.com>
2018-12-02 05:05:16 -0700

    Update counter.cpp

--- examples/counter/counter.cpp
+++ examples/counter/counter.cpp
@@ -1,6 +1,6 @@
 #include <systemc.h>
 
-SC_MODULE (first_counter) {
+SC_MODULE (counter) {
   sc_in_clk     clock ;      // Clock input of the design
   sc_in<bool>   reset ;      // active high, synchronous Reset input
   sc_in<bool>   enable;      // Active high enable signal for counter
@@ -29,7 +29,7 @@ SC_MODULE (first_counter) {
   // Since this counter is a positive edge trigged one,
   // We trigger the below block with respect to positive
   // edge of the clock and also when ever reset changes state
-  SC_CTOR(first_counter) {
+  SC_CTOR(counter) {
     cout<<"Executing new"<<endl;
     SC_METHOD(incr_count);
     sensitive << reset;
